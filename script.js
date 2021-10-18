b87528211805ab16b408686038dae73fb64750e1
Anais Kabambi <anais.k@hotmail.be>
2021-10-18 16:55:59 +0200

    Added functions to determine score, third card

--- script.js
+++ script.js
@@ -13,29 +13,32 @@ Blackjack.updates = document.getElementById("updates");
 Blackjack.suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
 Blackjack.values = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
     //adding values to Blackjack.values
-    let Ace = 1
-    let Two = 2
-    let Three = 3
-    let Four = 4
-    let Five = 5
-    let Six = 6
-    let Seven = 7
-    let Eight = 8
-    let Nine = 9
-    let Ten = 10
-    let Jack = 11
-    let Queen = 12
-    let King = 13
+    let Ace = 1; let Two = 2; let Three = 3; let Four = 4; let Five = 5; let Six = 6; let Seven = 7; let Eight = 8; let Nine = 9; let Ten = 10; let Jack = 11; let Queen = 12; let King = 13
+
+//display player score
+let x = document.getElementById("player-card1").value;
+let y = document.getElementById("player-card2").value;
+let z = document.getElementById("player-card3").value;
+document.getElementById("player-score").innerHTML = x + y + z;
+//display computer score
+let a = document.getElementById("computer-card1").value;
+let b = document.getElementById("computer-card2").value;
+let c = document.getElementById("computer-card3").value;
+document.getElementById("computer-score").innerHTML = a + b + c;
 
 //buttons
 document.getElementById("run").addEventListener("click",makeCard1);
 document.getElementById("run").addEventListener("click",makeCard2);
 document.getElementById("hit").addEventListener("click",addCard1,);
 document.getElementById("hit").addEventListener("click",addCard2);
+//document.getElementById("hit").addEventListener("click",addCard3);
+//document.getElementById("hit").addEventListener("click",addCard4);
 document.getElementById("player-card1").style.display = 'none';
 document.getElementById("player-card2").style.display = 'none';
+document.getElementById("player-card3").style.display = 'none';
 document.getElementById("computer-card1").style.display = 'none';
 document.getElementById("computer-card2").style.display = 'none';
+document.getElementById("computer-card3").style.display = 'none';
 
 //initial playercards
 function makeCard1() {
@@ -45,10 +48,10 @@ function makeCard1() {
     console.log(name)
 
     //displaying cards
-    function displayCardsP() {
-        let display = document.getElementById("player-card1").innerHTML = name;
+    function displayCards() {
+        document.getElementById("player-card1").innerHTML = name;
     }
-    displayCardsP();
+    displayCards();
 }
 makeCard1();
 
@@ -60,10 +63,10 @@ function makeCard2() {
     console.log(name)
 
     //displaying cards
-    function displayCardsP() {
-        let display = document.getElementById("computer-card1").innerHTML = name;
+    function displayCards() {
+        document.getElementById("computer-card1").innerHTML = name;
     }
-    displayCardsP();
+    displayCards();
 }
 makeCard2();
 
@@ -76,7 +79,7 @@ function addCard1() {
 
     //displaying cards
     function displayCardsP() {
-        let display = document.getElementById("player-card2").innerHTML = name;
+        document.getElementById("player-card2").innerHTML = name;
     }
     displayCardsP();
 }
@@ -91,7 +94,7 @@ function addCard2() {
 
     //displaying cards
     function displayCardsP() {
-        let display = document.getElementById("computer-card2").innerHTML = name;
+        document.getElementById("computer-card2").innerHTML = name;
     }
     displayCardsP();
 }
@@ -105,15 +108,29 @@ function showRun() {
 
 function showHit() {
     document.getElementById("player-card2").style.display = 'block';
+}
+
+function stand () {
     document.getElementById("computer-card2").style.display = 'block';
 }
 
 function addPlayerCards() {
-    if (makeCard1() + addCard1() === 21) {
+    if (x + y + z === 21) {
         Blackjack.updates.innerHTML = "Congratulations! You won!"
     }
-    if (makeCard1() + addCard1() > 21) {
+    if (x + y + z > 21) {
         Blackjack.updates.innerHTML = "Oh no! You're total is over 21!"
     }
 }
 addPlayerCards();
+
+//adding values of cards
+function addComputerCards () {
+    if (a + b + c === 21) {
+        Blackjack.updates.innerHTML = "Oh no! The computer has 21!"
+    }
+    if (a + b + c > 21) {
+        Blackjack.updates.innerHTML = "Congratulations! The computer's total is over 21!"
+    }
+}
+addComputerCards();
